14bdbb4b5ce91c36c5ad7eeafcfae59295ef0bd3
Sarah <lowpolyme@web.de>
2025-11-28 13:45:51 +0100

    use hue instead of rgb for color selection and saving

--- PathHighlightOverlay/Code/Settings/PathHighlightOptions.cs
+++ PathHighlightOverlay/Code/Settings/PathHighlightOptions.cs
@@ -1,3 +1,4 @@
+using ColossalFramework;
 using ColossalFramework.UI;
 using ICities;
 using UnityEngine;
@@ -11,7 +12,6 @@ namespace PathHighlightOverlay.Code.Settings
 
         public string Description =>
         "Highlights all pedestrian paths (including invisible ones).";
-
         
 
         public void OnSettingsUI(UIHelperBase helper)
@@ -27,19 +27,16 @@ namespace PathHighlightOverlay.Code.Settings
             label.text = "Highlight color";
 
             // HSB slider button
-            group.AddSlider("Highlight color", 0f, 1f, 1f, 0.5f, (value) => UpdateColor(value));
+            group.AddSlider("Highlight color", 0f, 1f, 0.1f, 0.5f, (value) => UpdateHue(value));
             //todo: preview the currently selected color by coloring a small box next to the slider
 
             // Reset button
             group.AddButton("Reset to default", () => { PathHighlightSettings.ResetToDefault(); });
         }
 
-        private void UpdateColor(float value)
+        private void UpdateHue(float value)
         {
-            Color color = Color.white;
-            //todo color slider conversion
-            //we don't care about saturation/brightness, we want a color hue slider and the values to be full brightness and fully saturated. We will achieve this with HSB to RGB conversion
-            PathHighlightSettings.HighlightColor = color;
+            PathHighlightSettings.SetHue(value);
         }
     }
 }
--- PathHighlightOverlay/Code/Settings/PathHighlightSettings.cs
+++ PathHighlightOverlay/Code/Settings/PathHighlightSettings.cs
@@ -6,38 +6,48 @@ namespace PathHighlightOverlay.Code.Settings
     public static class PathHighlightSettings
     {
         //todo GameSettings: 'PathHighlightOverlay' is not found or cannot be loaded. Make sure to call GameSettings.AddSettingsFile() to register a settings file before using SavedValue(). 
-        //todo store only HSV value and use Color.HSVToRGB(storedValue, 1f, 1f);
+
         public const string SETTINGS_FILE_NAME = "PathHighlightOverlay";
         
-        private static readonly SavedInt HighlightR =
-            new SavedInt("HighlightR", SETTINGS_FILE_NAME, 0, true);   
-        private static readonly SavedInt HighlightG =
-            new SavedInt("HighlightG", SETTINGS_FILE_NAME, 255, true); 
-        private static readonly SavedInt HighlightB =
-            new SavedInt("HighlightB", SETTINGS_FILE_NAME, 255, true); 
-        private static readonly SavedInt HighlightA =
-            new SavedInt("HighlightA", SETTINGS_FILE_NAME, 255, true); 
+        private static readonly SavedFloat _hue =
+            new SavedFloat("Hue", SETTINGS_FILE_NAME, 0f, true);
 
+        private static bool _registered;
+        public static void SetHue(float hue)
+        {
+            _hue.value = Mathf.Clamp01(hue);
+        }
+
+        public static float GetHue() => _hue.value;
+        
         public static Color HighlightColor
         {
-            get => new Color(
-                HighlightR.value / 255f,
-                HighlightG.value / 255f,
-                HighlightB.value / 255f,
-                HighlightA.value / 255f);
+            get
+            {
+                RegisterSettings();
+                return Color.HSVToRGB(_hue.value, 1f, 1f);;
+            }
 
             set
             {
-                HighlightR.value = Mathf.Clamp(Mathf.RoundToInt(value.r * 255f), 0, 255);
-                HighlightG.value = Mathf.Clamp(Mathf.RoundToInt(value.g * 255f), 0, 255);
-                HighlightB.value = Mathf.Clamp(Mathf.RoundToInt(value.b * 255f), 0, 255);
-                HighlightA.value = Mathf.Clamp(Mathf.RoundToInt(value.a * 255f), 0, 255);
+                Color.RGBToHSV(value, out float hue, out _, out _);
+                _hue.value = hue;
             }
         }
-
         public static void ResetToDefault()
         {
             HighlightColor = new Color(1f, 0f, 1f, 1f);
+        }
+
+        private static void RegisterSettings()
+        {
+            if (_registered) return;
+
+            var file = new SettingsFile();     
+            file.fileName = SETTINGS_FILE_NAME;
+            GameSettings.AddSettingsFile(file);
+
+            _registered = true;
         }
     }
 }
